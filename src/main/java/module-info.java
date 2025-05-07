272f4fc5a4e0363e4c0d98b12e8990c44e4f5a4f
Tobias Enger <tenger2s@smail.inf.h-brs.de>
2025-05-07 16:49:00 +0200

    init commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,17 @@
+module com.tobiasenger.daw.dawnana {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens com.tobiasenger.daw.dawnana to javafx.fxml;
+    exports com.tobiasenger.daw.dawnana;
+    exports com.tobiasenger.daw.dawnana.controller;
+    opens com.tobiasenger.daw.dawnana.controller to javafx.fxml;
+}
